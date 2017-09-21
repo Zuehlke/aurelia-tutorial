bee4bd0989dc194a7bdda0949df1bb1b58065513
Daniel Rosendorf <daniel.rosendorf@zuehlke.com>
2017-09-21 20:42:47 +0200

    Step 7: Result

--- aurelia-app/src/main.ts
+++ aurelia-app/src/main.ts
@@ -1,3 +1,4 @@
+import {I18N, Backend, TCustomAttribute} from 'aurelia-i18n';
 import {Aurelia} from 'aurelia-framework'
 import environment from './environment';
 
@@ -5,7 +6,27 @@ export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
     .feature('resources')
-    .plugin('aurelia-validation');
+    .plugin('aurelia-validation')
+    .plugin('aurelia-i18n', (instance) => {
+        let aliases = ['t', 'i18n'];
+        // add aliases for 't' attribute
+        TCustomAttribute.configureAliases(aliases);
+        
+        // register backend plugin
+        instance.i18next.use(Backend.with(aurelia.loader));
+
+        // adapt options to your needs (see http://i18next.com/docs/options/)
+        // make sure to return the promise of the setup method, in order to guarantee proper loading
+        return instance.setup({
+            backend: {                                  // <-- configure backend settings
+                loadPath: './locales/{{lng}}/{{ns}}.json', // <-- XHR settings for where to get the files from
+            },
+            attributes: aliases,
+            lng : 'de',
+            fallbackLng : 'en',
+            debug : false
+        });
+  });
 
   if (environment.debug) {
     aurelia.use.developmentLogging();
